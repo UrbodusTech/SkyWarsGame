5e934195b67ba5e85d872175bc84bc0a31a97e53
JoseLuisHD <joseluis.herrejon.developer@gmail.com>
2022-07-23 13:36:09 -0500

    [Build] bump version

--- src/main/java/com/skywars/GameLoader.java
+++ src/main/java/com/skywars/GameLoader.java
@@ -2,4 +2,14 @@ package com.skywars;
 
 import cn.nukkit.plugin.PluginBase;
 public class GameLoader extends PluginBase {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
